e3e03aaae18c8547ad048e74ecef7914cea0d613
Sweet-ka <sweet-ka@mail.ru>
2023-04-16 13:56:20 +0300

    feat: add decision

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,200 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
-
+  constructor(root_node) {
+    this.all_node = [];
+    if (root_node) this.root_node = this.#set_elem(root_node);
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.root_node) {
+      return this.root_node;
+    } else {
+      return null;
+    }
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  #set_elem(x) {
+    return { data: x };
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  #all_nodes(node) {
+    if (node.left) {
+      this.all_node.push(node.left.data);
+      this.#all_nodes(node.left);
+    }
+    if (node.right) {
+      this.all_node.push(node.right.data);
+      this.#all_nodes(node.right);
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (!data) return;
+    const find_free_ctx = find_free.bind(this);
+    this.root_node ? find_free_ctx(data, this.root_node) : (this.root_node = this.#set_elem(data));
+
+    function find_free(data, node) {
+      if (data < node.data) {
+        if (!node.left) {
+          node.left = this.#set_elem(data);
+        } else {
+          find_free_ctx(data, node.left);
+          return;
+        }
+      } else if (data > node.data) {
+        if (!node.right) {
+          node.right = this.#set_elem(data);
+        } else {
+          find_free_ctx(data, node.right);
+          return;
+        }
+      } else {
+        return;
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let search_this = search.bind(this);
+    let found = false;
+
+    if (this.root_node) {
+      search_this(data, this.root_node);
+      return found;
+    } else {
+      return false;
+    }
+
+    function search(data, node) {
+      if (data < node.data) {
+        if (!node.left) {
+          return;
+        } else {
+          search(data, node.left);
+        }
+      } else if (data > node.data) {
+        if (!node.right) {
+          return;
+        } else {
+          search(data, node.right);
+        }
+      } else {
+        found = true;
+        return;
+      }
+    }
+  }
+
+  find(data) {
+    let search_ctx = search.bind(this);
+    let found = null;
+
+    if (this.root_node) {
+      search_ctx(data, this.root_node);
+    }
+    return found;
+
+    function search(data, node) {
+      if (data < node.data) {
+        if (!node.left) {
+          found = null;
+          return;
+        } else {
+          search_ctx(data, node.left);
+        }
+      } else if (data > node.data) {
+        if (!node.right) {
+          found = null;
+          return;
+        } else {
+          search_ctx(data, node.right);
+        }
+      } else {
+        found = node;
+        return;
+      }
+    }
+  }
+
+  remove(data) {
+    let search_ctx = search.bind(this);
+    let parent = this;
+    let branch = "root_node";
+    this.all_node = [];
+
+    search_ctx(data, this.root_node);
+
+    // function all_nodes(node) {
+    //   if (node.left) {
+    //     all_node.push(node.left.data);
+    //     all_nodes(node.left);
+    //   }
+    //   if (node.right) {
+    //     all_node.push(node.right.data);
+    //     all_nodes(node.right);
+    //   }
+    // }
+
+    function search(data, node) {
+      if (data < node.data) {
+        branch = "left";
+        if (!node.left) {
+          return;
+        } else {
+          parent = node;
+          search_ctx(data, node.left);
+        }
+      } else if (data > node.data) {
+        branch = "right";
+
+        if (!node.right) {
+          return;
+        } else {
+          parent = node;
+          search_ctx(data, node.right);
+        }
+      } else {
+        this.#all_nodes(node);
+        delete parent[branch];
+        this.all_node.forEach((item) => {
+          this.add(item);
+        });
+        return;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.all_node = [];
+    let res = null;
+
+    if (this.root_node) {
+      this.all_node.push(this.root_node.data);
+      this.#all_nodes(this.root_node);
+      res = Math.min(...this.all_node);
+    }
+    return res;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.all_node = [];
+    let res = null;
+
+    if (this.root_node) {
+      this.all_node.push(this.root_node.data);
+      this.#all_nodes(this.root_node);
+      res = Math.max(...this.all_node);
+    }
+    return res;
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
